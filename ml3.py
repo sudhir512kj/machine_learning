1542028ad0e7eabcea1c149b493225391b293568
sudhir512kj <sureshmeena512@gmail.com>
2017-09-07 04:59:37 +0530

    third commit

--- ml3.py
+++ ml3.py
@@ -1,34 +1,82 @@
 # breast cancer class prediction
 
 import numpy as np
-from sklearn import preprocessing, cross_validation, neighbors
+from collections import Counter
 import pandas as pd
+import warnings
+import random
+import matplotlib.pyplot as plt
+from matplotlib import style
 from math import sqrt
+
+style.use('fivethirtyeight')
+
+def k_nearest_neighbors(data, predict, k=3):
+    if len(data) >= k:
+        warnings.warn('K is set to a value less than total voting groups!')
+
+    distances = []
+    for group in data:
+        for features in data[group]:
+            euclidean_distance = np.linalg.norm(np.array(features)-np.array(predict))
+            distances.append([euclidean_distance, group])
+
+    votes = [i[1] for i in sorted(distances)[:k]]
+    vote_result = Counter(votes).most_common(1)[0][0]
+    confidence = Counter(votes).most_common(1)[0][1]/k
+
+    return vote_result, confidence
+
 
 df = pd.read_csv('breast-cancer-wisconsin.data')
 df.replace('?', -99999, inplace=True)
 df.drop(['id'], 1, inplace=True)
+full_data = df.astype(float).values.tolist()
 
-X = np.array(df.drop(['class'], 1))
-y = np.array(df['class'])
+random.shuffle(full_data)
+test_size = 0.2
+train_set = {2:[], 4:[]}
+test_set = {2:[], 4:[]}
+train_data = full_data[:-int(test_size*len(full_data))]
+test_data = full_data[-int(test_size*len(full_data)):]
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+for i in train_data:
+    train_set[i[-1]].append(i[:-1])
 
-clf = neighbors.KNeighborsClassifier()
+for i in test_data:
+    test_set[i[-1]].append(i[:-1])
 
-clf.fit(X_train, y_train)
+correct = 0
+total = 0
 
-accuracy =  clf.score(X_test, y_test)
-print(accuracy)
+for group in test_set:
+    for data in test_set[group]:
+        vote, confidence = k_nearest_neighbors(train_set, data, k=5)
+        if group == vote:
+            correct += 1
+        total += 1
+print('Accuracy:', correct/total)
 
-example_measures = np.array([[4,2,1,1,1,2,3,2,1], [4,2,1,1,1,2,3,2,1]])
-example_measures = example_measures.reshape(len(example_measures), -1)
+# X = np.array(df.drop(['class'], 1))
+# y = np.array(df['class'])
 
-prediction = clf.predict(example_measures)
-print(prediction)
+# X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
-plot1 = [1,3]
-plot2 = [2,5]
+# clf = neighbors.KNeighborsClassifier()
 
-euclidean_distance = sqrt( (plot1[0]-plot2[0])**2 + (plot1[1]-plot2[1])**2 )
-print(euclidean_distance)
+# clf.fit(X_train, y_train)
+
+# accuracy =  clf.score(X_test, y_test)
+# print(accuracy)
+
+# example_measures = np.array([[4,2,1,1,1,2,3,2,1], [4,2,1,1,1,2,3,2,1]])
+# example_measures = example_measures.reshape(len(example_measures), -1)
+
+# prediction = clf.predict(example_measures)
+# print(prediction)
+
+# plot1 = [1,3]
+# plot2 = [2,5]
+
+# euclidean_distance = sqrt( (plot1[0]-plot2[0])**2 + (plot1[1]-plot2[1])**2 )
+# print(euclidean_distance)
